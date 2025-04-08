af3686f190eed458ae462c2831f6e7b9d67a4c80
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-04-08 22:57:11 +0700

    Them tinh nang edit, add customer, modal edit va add va css app + minh chung

--- Tuan07/tuan07/src/App.jsx
+++ Tuan07/tuan07/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import AdminPage from './components/AdminPage.jsx'
-import Navbar from './components/Navbar.jsx'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import Dashboard from './components/Dashboard.jsx';
+import { useRef, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Dashboard from "./components/Dashboard.jsx";
+import Projects from "./components/Projects.jsx";
+import Teams from "./components/Teams.jsx";
+import Analytics from "./components/Analytics.jsx";
 
 function App() {
   return (
     <>
-
-
-<BrowserRouter>
+      <BrowserRouter>
         <div className="m-auto w-11/12 grid grid-cols-4 gap-0">
           <Navbar />
           <Routes>
@@ -30,12 +35,35 @@ function App() {
                 </div>
               }
             />
+            <Route
+              path="/projects"
+              element={
+                <div className="border-t-2 border-r-2 border-b-2 border-gray-200 col-span-3">
+                  <Projects />
+                </div>
+              }
+            />
+            <Route
+              path="/teams"
+              element={
+                <div className="border-t-2 border-r-2 border-b-2 border-gray-200 col-span-3">
+                  <Teams />
+                </div>
+              }
+            />
+            <Route
+              path="/analytics"
+              element={
+                <div className="border-t-2 border-r-2 border-b-2 border-gray-200 col-span-3">
+                  <Analytics />
+                </div>
+              }
+            />
           </Routes>
         </div>
       </BrowserRouter>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
