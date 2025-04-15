86df54f0d644fcc69275bc8107bcd8c2c7f747d8
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-04-15 21:07:55 +0700

    Dung AI tao header web + minh chung

---
+++ TH_Tuan08/bai2/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Header.jsx';
+import Hero from './Hero.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero></Hero>
+      {/* Phần nội dung khác */}
+    </>
+  );
+}
+
+export default App
